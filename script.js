35993270e242db6b48e75c6062bd1d34baf32afd
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-08-22 04:52:22 +0500

    done with the imgs

--- script.js
+++ script.js
@@ -253,6 +253,30 @@ allSections.forEach(function (section) {
   section.classList.add("section--hidden");
 });
 ///////////////////////////////////////
+// lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]");
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  //replacing src with data-src after loading (.dataset.src)
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+//Tell observer to watch each lazy-loaded image
+imgTargets.forEach((img) => imgObserver.observe(img));
+
+///////////////////////////////////////
 // Learning
 
 // // 1. for selecting the elements
